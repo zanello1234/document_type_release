725c486a7abe17c650d6267dccb244dd47a2b84f
Martin Zanello <91170739+zanello1234@users.noreply.github.com>
2026-01-21 15:28:55 -0300

    Delete document_type_release directory

--- document_type_release/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-"""Models for Document Type Release module."""
-
-from . import l10n_latam_document_type
-from . import account_move
