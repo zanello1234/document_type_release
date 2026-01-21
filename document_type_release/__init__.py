2674b9f9e33ea3c422b5c9766075e3c182a0fd85
Martin Zanello <91170739+zanello1234@users.noreply.github.com>
2026-01-21 15:31:24 -0300

    Add files via upload

---
+++ document_type_release/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+"""Document Type Release - LATAM Localization.
+
+This module provides tools to manage LATAM document type availability,
+allowing administrators to activate/deactivate document types and force
+their availability regardless of AFIP restrictions.
+"""
+
+from . import models
